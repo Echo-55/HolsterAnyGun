110553641726106d28a8c28905cfaa970d9bd73e
Echo-55 <82003117+Echo-55@users.noreply.github.com>
2024-07-15 10:27:48 -0500

    Update to 3.9.1 and cleanup

--- src/mod.ts
+++ src/mod.ts
@@ -6,19 +6,14 @@ import { DependencyContainer } from "tsyringe";
 
 class HolsterAnyGun implements IPostDBLoadMod {
     modName = "HolsterAnyGun";
-
     public postDBLoad(container: DependencyContainer): void {
         const logger = container.resolve<ILogger>("WinstonLogger");
         logger.logWithColor(`${this.modName} - Holster any gun enabled`, LogTextColor.CYAN)
-
         const dataBaseTables = container.resolve<DatabaseServer>("DatabaseServer").getTables();
         const items = dataBaseTables.templates.items;
-
         const inventory = items["55d7217a4bdc2d86028b456d"]
-        const holster = inventory._props.Slots[2]
-
+        const holster = inventory._props.Slots.find((slot: any) => slot._name === "Holster");
         holster._props.filters[0].Filter.push("5422acb9af1c889c16000029");
     }
 }
-
 module.exports = { mod: new HolsterAnyGun() }
